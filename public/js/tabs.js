8c72c65b194c72222db5c2bcfc64338169695b26
Xenapsis <149744857+Xenapsis@users.noreply.github.com>
2024-05-12 01:55:56 -0400

    pain...

--- public/js/tabs.js
+++ public/js/tabs.js
@@ -11,22 +11,44 @@ function tab1() {
 function closetabs(e) {
     let x = e.target.id.substring(1)
     let x2 = x - 1
-    console.log(x)
+    let tabholder = document.querySelectorAll(".tabholder")
+    let tabs = document.querySelectorAll(".tab")
+    let Xs = document.querySelectorAll(".X")
     document.getElementById("tabholder" + x).remove()
     document.getElementById("iframe" + x).remove()
+    var iframes = document.getElementsByTagName('iframe');
+    for (var i = 0; i < iframes.length; i++) {
+        iframes[i].style.display = 'none'
+    }
+    for (var i = 0; i < tabholder.length; i++) {
+        tabholder[i].id = "tabholder" + (i);
+        if (tabholder[i].id === "tabholder0") {
+            tabholder[i].id = "tabholder1"
+        }
+    }
+    for (var i = 0; i < tabs.length; i++) {
+        tabs[i].id = "tab" + (i);
+        if (tabs[i].id === "tab0") {
+            tabs[i].id = "tab1"
+        }
+    }
+    for (var i = 0; i < Xs.length; i++) {
+        Xs[i].id = "X" + (i);
+        if (Xs[i].id === "X0") {
+            Xs[i].id = "X1"
+        }
+    }
     document.getElementById("iframe" + x2).style.display = "block"
 }
 
 function switchtab(e) {
     var tab = e.target.id
     var tabnum = tab.substring(3);
-    console.log(tabnum)
     var iframes = document.getElementsByTagName('iframe');
     for (var i = 0; i < iframes.length; i++) {
         iframes[i].style.display = 'none'
     }
     var iframeid = "iframe" + tabnum
-    console.log(iframeid)
     var iframe = document.getElementById(iframeid)
     iframe.style.display = "block"
     
@@ -35,26 +57,19 @@ function switchtab(e) {
 function createtab(e) {
     var iframes = document.getElementsByTagName('iframe');
 
-// Loop through each iframe
+
     for (var i = 0; i < iframes.length; i++) {
         iframes[i].style.display = 'none'
     }
-    let iframe = document.createElement('iframe');
-    let openiframe
-    iframe.src = "/proxy.html"
-    iframe.classList.add('flex', 'justify-center', 'items-center')
-    iframe.style.width = '90%';
-    iframe.style.height = '100vh';
-    let framecounter = iframes.length + 1;
-    iframe.setAttribute("id", "iframe" + framecounter)
-    document.getElementById('iframecontainer').appendChild(iframe);
+    let framelen = iframes.length
+    const iframe1 = framelen + 1
     let tabholder = document.createElement('div')
     tabholder.setAttribute('class', 'tabholder' + iframes.length)
     tabholder.setAttribute("class", "tabholder")
     tabholder.setAttribute('id', 'tabholder' + iframes.length)
     document.getElementById('buttons').appendChild(tabholder)
     let newtab = document.createElement('button');
-    newtab.innerHTML = "New Tab " + iframes.length ++;
+    newtab.innerHTML = "New Tab " + iframes.length;
     newtab.onclick = switchtab;
     newtab.setAttribute("id", "tab" + iframes.length)
     newtab.setAttribute("class", "tab")
@@ -75,19 +90,27 @@ function createtab(e) {
     newplus.setAttribute("class", "plus")
     newplus.onclick = createtab;
     document.getElementById('buttons').appendChild(newplus)
-
+    console.log(iframe1)
+    let iframe = document.createElement('iframe');
+    iframe.src = "/proxy.html"
+    iframe.classList.add('flex', 'justify-center', 'items-center')
+    iframe.style.width = '90%';
+    iframe.style.height = '100vh';
+    iframe.setAttribute("id", "iframe" + iframes.length)
+    iframe.setAttribute("class", "rounded-xl")
+    iframe.setAttribute("class", "proxyframe")
+    document.getElementById('iframecontainer').appendChild(iframe);
 };
 
 setInterval(() => {
-    let allframes = document.getElementsByTagName('iframe');
-    for (var i = 0; i < allframes.length; i++) {
-        const iframedoc = allframes[i].contentDocument
-        const iframewin = allframes[i].contentWindow.document
-        let iframeid = allframes[i].id.substring(6)
-        console.log(iframeid)
-        let tab = document.getElementById("tab" + iframeid)
-        let search = iframedoc.getElementById('search')
-        tab.innerHTML = iframedoc.title
+    let alltabs = document.querySelectorAll(".tab")
+    let allframes = document.getElementsByTagName("iframe")
+    for (var i = 0; i < alltabs.length; i++) {
+        const iframedoc = allframes[i + 1].contentDocument
+        if (allframes[i] != document.getElementById('frame0')) {
+            let tab = document.getElementById("tab" + (i + 1))
+            console.log(i + 1)
+            tab.innerHTML = iframedoc.title
+        }
     }
 }, 1000);
-
